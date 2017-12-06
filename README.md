42062e37cc27826c574435d882b9e371dbb80445
Adam Wozniak <adam2hp@gmail.com>
2017-12-06 21:36:43 +0100

    Change readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This repo contains all code used in Optimization course. Select branch to see only specyfic labolatories. Master branch contains all labolatries merged in one project
 
 Lab1: "Optymalizacja funkcji jednej zmiennej metodami bezgradientowymi" / "One variable function optimization using gradientless optimization methods"
+
+Lab2: "Optymalizacja funkcji wielu zmiennych metodami bezgradientowymi" / "Multivariable function optimization using gradientless optimization methods"
--- lab2/lab2spraw.m
+++ lab2/lab2spraw.m
@@ -1,52 +1,47 @@
 clear all; close all; clc;
 
-expanceIndicators = [1.000789,1.5000094,1.90008776];
+stepsArr = [0.15,0.45,0.65];
 Nmax=1000;
 epsilon = 1e-3;
+beta=0.5;
 
-x_opt_lag_vec = [];
-y_opt_lag_vec = [];
+x_opt_HJ_vec = {};
+y_opt_HJ_vec = [];
 
-x_opt_fib_vec = [];
-y_opt_fib_vec = [];
-
-
-w_exp_vec = [];
-w_fib_vec = [];
-w_lag_vec = [];
+x_opt_ros_vec = {};
+y_opt_ros_vec = [];
 
 
-x0_vec = [];
-a_vec = [];
-b_vec = [];
+w_HJ_vec = [];
+w_ros_vec = [];
+
+
+x0_vec = {};
+
+
+alfaHJ=0.5;
+alfaROSEN=2;
  
 
-
-for e=expanceIndicators
+for e=stepsArr
   for i=1:100
-    x0 = 199*rand-100;
+    x0 = 2*rand(2,1)-1;
     
     x0_vec(end+1) = x0;
     
     x1=x0+1;
-    [a,b,w] = expance(x0,x1,e,Nmax);
     
-    a_vec(end+1) = a;
-    b_vec(end+1) = b;
+    [x_opt_HJ,y_opt_HJ,w_HJ] = HJ(x0,e,alfaHJ,epsilon,Nmax)
     
-    w_exp_vec(end+1) = w;
+    x_opt_HJ_vec(end+1) = x_opt_HJ;
+    y_opt_HJ_vec(end+1) = y_opt_HJ;
+    w_HJ_vec(end+1) = w_HJ;
     
-    [x_opt_lag,y_opt_lag,w_lag] = lag(a,b,epsilon,1000);
+    [x_opt_ros,y_opt_ros,w_ros] = Rosen (x0,e,alfaROSEN,beta,epsilon,Nmax)
     
-    x_opt_lag_vec(end+1) = x_opt_lag;
-    y_opt_lag_vec(end+1) = y_opt_lag;
-    w_lag_vec(end+1) = w_lag;
-    
-    [x_opt_fib,y_opt_fib,w_fib] = fib(a,b,epsilon);
-    
-    x_opt_fib_vec(end+1) = x_opt_fib;
-    y_opt_fib_vec(end+1) = y_opt_fib;
-    w_fib_vec(end+1) = w_fib;
+    x_opt_ros_vec(end+1) = x_opt_ros;
+    y_opt_ros_vec(end+1) = y_opt_ros;
+    w_ros_vec(end+1) = w_ros;
     
     end
 end
